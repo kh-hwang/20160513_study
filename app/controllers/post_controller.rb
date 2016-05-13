0d53f6af68300ce836d1e8520ebfd681a53e621f
khhwangv <khhwangv@gmail.com>
2016-05-13 17:40:09 +0900

    reply model and controller

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,4 +1,11 @@
 class PostController < ApplicationController
   def index
   end
+
+  def reply_write
+    reply = Reply.new
+    reply.content = params[:content]
+    reply.post_id = params[:id]
+  end
+
 end
--- db/migrate/20160513082700_create_replies.rb
+++ db/migrate/20160513082700_create_replies.rb
@@ -1,7 +1,8 @@
 class CreateReplies < ActiveRecord::Migration
   def change
     create_table :replies do |t|
-
+      t.string :content
+      t.integer :post_id
       t.timestamps null: false
     end
   end
