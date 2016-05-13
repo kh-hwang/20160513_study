ad7e867ece393f9da6c207207c7ee812b92f99ea
ChangHee <leechhe90@gmail.com>
2016-05-13 17:36:16 +0900

    migrate db

--- db/migrate/20160513082700_create_replies.rb
+++ db/migrate/20160513082700_create_replies.rb
@@ -1,7 +1,8 @@
 class CreateReplies < ActiveRecord::Migration
   def change
     create_table :replies do |t|
-
+      t.integer :post_id
+      t.text :content
       t.timestamps null: false
     end
   end
