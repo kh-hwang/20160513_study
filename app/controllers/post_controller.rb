e5128d1629fbed73b7f74b47f2959f95942eb251
Wooru <yjh3602@gmail.com>
2016-05-13 17:58:20 +0900

    데이터베이스 저장 함수 작성

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,6 +1,14 @@
 class PostController < ApplicationController
   def index
+    indexpost = Post.all.reverse
 
+  end
+
+  def write
+    newpost = Post.new
+    newpost.title = params[:title]
+    newpost.content = params[:content]
+    newpost.save
   end
 
   def reply_write
